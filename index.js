8d04e0419d9912ba202fe4cf35e17b98900506de
King Bogo <josephogbo111@gmail.com>
2023-03-22 20:35:57 +0100

    added js file and toggle navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+const close = document.querySelector(".close");
+
+menu.onclick = () => {
+  navbar.classList.toggle("open");
+  //   if(navbar.classList.contains("open"){
+
+  //   })
+};
