05618c755276a79a974ffe1454a48cde332ffa5f
King Bogo <josephogbo111@gmail.com>
2023-03-22 21:09:36 +0100

    removed unnecessary console log in js file

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
 const closeBtn = document.querySelector(".close");
 
-console.log(close.style);
-
 hamburger.onclick = () => {
   navbar.classList.add("open");
   hamburger.classList.add("hide");
